5d0506b1d1c3a7bf6a02bcdbc80216743117a119
QuentinLeFourbe <quentingarcia40@gmail.com>
2021-07-26 09:12:37 +0200

    footer ongoing

--- src/components/Base/Footer.jsx
+++ src/components/Base/Footer.jsx
@@ -1,35 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { useSpring, animated, config } from 'react-spring'
+import VisiblitySensor from 'react-visibility-sensor';
 
 const Container = styled(animated.footer)`
     grid-area: footer;
+    min-height: 20vh;
     background-color: #18534F;
-    min-height: 10vh;
     display: flex;
-    justify-content: center;
+    flex-flow: row wrap;
+    justify-content: space-around;
     align-items: center;
 `;
 
+const AnimatedContent = styled(animated.div)`
+    
+`;
+
 const FooterLink = styled(Link)`
- 
- color: white;
+    font-family: "Times New Roman", Times, serif;
+    color: white;
+    font-size: 3rem;
+    margin: 1rem;
+    text-decoration: none;
+`;
+
+const MailLink = styled.a`
+    font-family: "Times New Roman", Times, serif;
+    color: white;
+    margin: 1rem;
+    font-size: 3rem;
+    text-decoration: none;
 `;
 
 function Footer() {
+    const [isVisible, setIsVisible] = useState(false);
+
     const springProps = useSpring({
-        from: { y: 50, opacity: 0 },
-        to: { y: 0, opacity: 1 },
-        config: config.tight,
-    })
+        from: {
+            y: 100,
+            opacity: 0,
+        },
+        y: isVisible ? 0 : 100,
+        opacity: isVisible ? 1 : 0,
+        config: config.molasses,
+    });
+
+    const onChangeVisibility = (isVisible) => {
+        if (isVisible)
+            setIsVisible(true);
+    }
 
     return (
-        <Container style={springProps}>
-            <FooterLink to="/about">
-                A propos
-            </FooterLink>
-        </Container>
+        <VisiblitySensor onChange={onChangeVisibility} >
+            <Container >
+                <AnimatedContent style={springProps}>
+                    <MailLink href="mailto:email@example.com">Contactez-moi !</MailLink>
+                </AnimatedContent>
+            </Container>
+        </VisiblitySensor>
     )
 }
 
